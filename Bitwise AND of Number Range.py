105ffb702331259c1d66f78b971524c3148fa960
Aishika Vishwakarma <100345686+Aish-ika031@users.noreply.github.com>
2026-06-23 22:23:56 +0530

    Create Bitwise AND of Number Range.py

---
+++ Bitwise AND of Number Range.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        
+        cnt = 0
+
+        while left != right:
+
+            cnt += 1
+
+            left , right = left >> 1 , right >> 1
+
+        return left << cnt
