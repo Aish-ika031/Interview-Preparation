bbdce0a215606c0ce18b9b9f457bcfa42277a1d1
Aishika Vishwakarma <100345686+Aish-ika031@users.noreply.github.com>
2026-08-16 22:56:29 +0530

    Create Nearest Available Drone.py

---
+++ Nearest Available Drone.py
@@ -0,0 +1,15 @@
+class Solution:
+    def nearestDrone(self, drones: list[list[int]], target: list[int]) -> int:
+        tx, ty = target
+
+        min_dist = float('inf')
+        ans = -1
+
+        for i, (x, y, r) in enumerate(drones):
+            dist = abs(x - tx) + abs(y - ty)
+
+            if dist <= r and dist < min_dist:
+                min_dist = dist
+                ans = i
+
+        return ans
