01973e5aac64c4eb4616a25a991b3d70e2834c97
Aishika Vishwakarma <100345686+Aish-ika031@users.noreply.github.com>
2026-08-30 22:52:43 +0530

    Create Removing Minimum and Maximum From Array.py

---
+++ Removing Minimum and Maximum From Array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minimumDeletions(self, nums):
+        n = len(nums)
+
+        minIndex = nums.index(min(nums))
+        maxIndex = nums.index(max(nums))
+
+        left = min(minIndex, maxIndex)
+        right = max(minIndex, maxIndex)
+
+        front = right + 1
+        back = n - left
+        both = (left + 1) + (n - right)
+
+        return min(front, back, both)
